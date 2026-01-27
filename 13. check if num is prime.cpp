8e44c988f75748d5d281ce6281f21964081d3822
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-01-27 09:17:37 +0530

    Create 13. check if num is prime.cpp

---
+++ 13. check if num is prime.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool isprime(int x){
+    if(x<=1)return false;
+    if(x%2==0)return false;
+    for(int i=3;i*i<x;i+=2){
+        if(x%i==0)return false;
+    }
+    return true;
+}
+int main(){
+    int n;
+    cin>>n;
+    if(isprime(n))cout<<n<<" is a prime.";
+    else cout<<n<<" is not prime.";
+    
+}
