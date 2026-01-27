de4d5affcd16ecb1bdc2b567712a85336b775ee7
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-01-27 10:30:48 +0530

    Update 14. (Sieve of Eratosthenes) check all num less then n if prime or not.cpp

--- 14. (Sieve of Eratosthenes) check all num less then n if prime or not.cpp
+++ 14. (Sieve of Eratosthenes) check all num less then n if prime or not.cpp
@@ -1,5 +1,5 @@
 //Sieve of Eratosthenes
-
+// Time: O(nlog(log(n)))
 int main(){
     int n;
     cin>>n;
@@ -8,7 +8,7 @@ int main(){
     for(int i=2;i*i<=n;i++){
       if(arr[i]){ // only marking the multiples of primes to false because:
                    // fundamental therorem of arithmetic: Every composite number \(C\) has at least one prime factor \(p\) such that \(p<C\).
-        for(int j=i+i;j<=n;j+=i){
+        for(int j=i*i;j<=n;j+=i){
             arr[j]=false; // marking the multiples of prime numbers to non primes 
         }
       }
