b23246ff16e2f5c59b17aa1f37e35b5767461525
Yashwant <139669859+yashwant-gh@users.noreply.github.com>
2026-01-29 10:00:05 +0530

    Create 24. print all sub-sequence using bit manipulation.cpp

---
+++ 24. print all sub-sequence using bit manipulation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++)cin>>arr[i];
+    
+    
+    for(int i=0;i<(1<<n);i++){  //1<<n: if n=1 it is 2 if n=0 it is 1
+        for(int j=0;j<n;j++){
+            if(i&(1<<j))cout<<arr[j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
